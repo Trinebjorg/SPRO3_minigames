d53b1df5239b6a24b5e52a683aee26848a3dd664
Trinebjorg <trini20@student.sdu.dk>
2021-11-21 11:39:43 +0100

    Memory_2

--- memory test 2/Assets/Scripts/BackToMain.cs
+++ memory test 2/Assets/Scripts/BackToMain.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
 
 public class BackToMain : MonoBehaviour
 {
     //Coding for Addbuttons
 
-    [SerializeField]
-    private Transform MainField_Panel;
+    //   [SerializeField]
+    //   private Transform MainField_Panel;
+    //   [SerializeField]
+    //   private GameObject Exit;
+    //   [SerializeField]
+    //   private GameObject Break;
+    public Button btn;
 
-    [SerializeField]
-    private GameObject Exit;
+    public void SceneChangeOnClick() {
+       
 
-    [SerializeField]
-    private GameObject Break;
+        //change scene for Exit_button
+        if (btn != null) {
+            if (btn.name == "Exit_Button") {
+                Application.Quit();
+            }
 
-
+        }
+    }
 }

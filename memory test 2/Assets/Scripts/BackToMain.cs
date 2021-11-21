b84e58c25086277f63b87cf7d383ed9864677961
Trinebjorg <trini20@student.sdu.dk>
2021-11-21 11:20:03 +0100

    Scene_main

---
+++ memory test 2/Assets/Scripts/BackToMain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMain : MonoBehaviour
+{
+    //Coding for Addbuttons
+
+    [SerializeField]
+    private Transform MainField_Panel;
+
+    [SerializeField]
+    private GameObject Exit;
+
+    [SerializeField]
+    private GameObject Break;
+
+
+}

55b43080ad8a4a6bd8bac230d85c06612607e279
Dimi051100 <dimitar_ilev@t-online.de>
2021-11-25 00:58:01 +0100

    HandsExtra

---
+++ memory test 2/Assets/Scripts/HandController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(ActionBasedController))]
+public class HandController : MonoBehaviour
+{
+    ActionBasedController controller;
+    
+    public Hand hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>(); // Get the controller
+    }
+
+    // Update is called once per frame
+    void Update() // Get Trigger and Grip input
+    {
+        hand.SetGrip(controller.selectAction.action.ReadValue<float>());
+        hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
+    }
+}

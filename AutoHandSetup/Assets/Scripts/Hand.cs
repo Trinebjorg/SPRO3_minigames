1c7d942dafef95c6c58a08d867bbf7bc27a623af
Dimi051100 <dimitar_ilev@t-online.de>
2021-12-08 09:29:31 +0100

    Test123

---
+++ AutoHandSetup/Assets/Scripts/Hand.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    public float Speed;
+
+    Animator animator;
+    SkinnedMeshRenderer mesh;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+  
+    private String animatorGripParam = "Grip";
+    private String animatorTriggerParam = "Trigger";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>(); // Mesh around objects
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand(); // call every frame
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * Speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+        
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * Speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        mesh.enabled = !mesh.enabled; // Takes value inside mesh.en -> flips it around -> sets it
+    }
+}

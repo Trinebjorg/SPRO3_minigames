c322d07c1f8a53c7c16e7fcb66b468169b700218
Dimi051100 <dimitar_ilev@t-online.de>
2021-12-08 12:27:22 +0100

    Try123

--- memory test 2/Assets/Scripts/Add_buttons.cs
+++ memory test 2/Assets/Scripts/Add_buttons.cs
@@ -6,10 +6,8 @@ public class Add_buttons : MonoBehaviour
 {
 
     // accessing the difficulty declared when selecting difficulty
- 
-   /* [SerializeField]
-    private Difficulty Diff; 
-   */
+    Difficulty Diff; 
+
     [SerializeField]
     private Transform puzzleField;
 
@@ -18,7 +16,7 @@ public class Add_buttons : MonoBehaviour
 
     private void Awake()
     {
-        for (int i = 0; i < /*Diff.btn_count*/ 8 ; i++)
+        for (int i = 0; i < /*Diff.btn_count*/ 8; i++)
         {
             GameObject button = Instantiate(btn);
             button.name = "" + i;

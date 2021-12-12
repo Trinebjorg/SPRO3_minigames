3ece3b6bf796050fd780ff71dc49d963e0fdc048
Trinebjorg <trini20@student.sdu.dk>
2021-12-12 14:13:33 +0100

    clover

---
+++ memory test 2/Assets/saber.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saber : MonoBehaviour
+{
+    public LayerMask Layer;
+    private Vector3 previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, Layer))
+        {
+            if (Vector3.Angle(transform.position-previousPos, hit.transform.up)>130) //Hit the objects over 130 degrees
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        previousPos = transform.position;
+    }
+}
--- memory test 2/Assets/spawner.cs
+++ memory test 2/Assets/spawner.cs
@@ -20,9 +20,13 @@ public class spawner : MonoBehaviour
     {
         if(timer>beat)
         {
-            GameObject cube = Instantiate(cubes[Random.Range(0, 4)], points[Random.Range(0, 4)]);
+            GameObject cube = Instantiate(cubes[Random.Range(0, 4)], points[Random.Range(0, 4)]); //point = transform?
             cube.transform.localPosition = Vector3.zero;
             cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 2));
+            // Rotate the object around its local X axis at 1 degree per second:
+            //cube.transform.Rotate(Time.deltaTime, 0, 0);
+            // ...also rotate around the World's Y axis
+           // transform.Rotate(0, Time.deltaTime, 0, Space.World);
             timer -= beat;
         }
         timer += Time.deltaTime;

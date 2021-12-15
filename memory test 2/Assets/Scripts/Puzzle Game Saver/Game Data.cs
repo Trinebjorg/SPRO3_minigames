fa70546e56fab37fb6aaf74b3380a7fe2cb4f7d6
jafib19 <Jafib19@student.sdu.dk>
2021-12-15 11:50:16 +0100

    dududd

---
+++ memory test 2/Assets/Scripts/Puzzle Game Saver/Game Data.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class GameData
+{
+    private bool cloverPuzzleLevels;
+    private bool marvelPuzzleLevels;
+    private bool transportPuzzleLevels;
+
+    private int[] cloverPuzzleLevelStars;
+    private int[] marvelPuzzleLevelStars;
+    private int[] transportPuzzleLevelStars;
+
+    private bool isTheGameStartedForTheFirstTime;
+
+    private float musicVolume;
+
+    public void SetMusicVolume(float musicVolume)
+    {
+        this.musicVolume = musicVolume;
+    }
+
+    public float GetMusicVolume()
+    {
+        return this.musicVolume;
+    }
+
+
+
+}
---
+++ memory test 2/Assets/Scripts/Puzzle Music Controller/MusicController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    [SerializeField]
+    private PuzzleGameManager puzzleGameSaver;
+
+    private AudioSource bgMusicClip;
+
+    private float musicVolume;
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void GetAudioSource()
+    {
+
+
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+
+    }
+
+    void PlayOrTurnOffMusic(float volume)
+    {
+
+    }
+
+
+}
